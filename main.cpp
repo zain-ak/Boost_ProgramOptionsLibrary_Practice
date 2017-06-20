676e8ef8bd89676ad626491b002a0ab88943de72
Zain Khalid <zak565@uowmail.edu.au>
2017-06-20 05:49:31 -0400

    Added Customization
    Followed Customization tutorial and added that to the code as well.

--- main.cpp
+++ main.cpp
@@ -10,15 +10,27 @@ namespace po = boost::program_options; //use namespace more easily
 */
 
 int main (int argc, char* argv[]) {
+    
+    int testVar;
+    
     po::options_description desc("Program options");
     desc.add_options()
-            ("info", "This is a test for program options")
-            ("help", "Use this command to view help")
-            ("filename", po::value<string>()->default_value("default.txt"), "file to process");
+            ("filename,F", po::value<string>()->default_value("default.txt"), "file to process") //note how default value is assigned
+            ("inttest,S", po::value<int>(&testVar)->default_value(7), "testing ints") //Make sure there's no space in first parameter string
+            ("multiples,M", po::value<vector<string>>()->multitoken(), "Adding multiple parameters"); //Add multitoken here if you have multiple parameters after the command
 
+    /*If you want to use positional options, i.e not add flags: specify additional descriptor
+      Positional arguments go before flagged arguments in terminal
+     */
+    po::positional_options_description pos;
+    pos.add("multiples", -1); //-1 indicates unlimited parameters
+    
+    
     po::variables_map vm;
-    auto parsed = po::parse_command_line(argc, argv, desc);
-    po::store(parsed, vm);
+    //auto parsed = po::parse_command_line(argc, argv, desc); //instead of this, with positional options descriptor you have to manually specify
+    po::command_line_parser parser(argc, argv);
+    auto parsed = parser.options(desc).positional(pos).run();
+    po::store(parsed, vm); 
     po::notify(vm);
     
     if (vm.count("info")) {
@@ -26,12 +38,20 @@ int main (int argc, char* argv[]) {
         return 1;
     }
     
-    if (vm.count("filename")) {
+    if (vm.count("filename") || vm.count("inttest")) { //This if will run since filename and inttest have been assigned a default value
         cout << "File found: " << vm["filename"].as<string>() << endl;
-        return 1;
+        cout << "Test Integer found: " << vm["inttest"].as<int>() << endl;
     }
     
-    cout << "File found: default.txt" << endl;
+    if (vm.count("multiples")) {
+        cout << "\nMultiple filenames found:\n";
+        auto files = vm["multiples"].as<vector<string>>();
+        for (auto i : files) {
+            cout << i << endl;
+        }
+    }
+    else
+        cout << "\nNo multiple files specified\n";
     
     return 0;
 }
